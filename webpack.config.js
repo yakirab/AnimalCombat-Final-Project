ad2b560d2b611f389d56658c27c7db5fd60729ca
yakirab <108821540+yakirab@users.noreply.github.com>
2025-08-17 16:01:04 +0300

    Remove unused react-native-reanimated dependency and add webpack config to suppress useNativeDriver warnings on web

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
-module.exports = async function(env, argv) {
+module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
+  
+  // Suppress useNativeDriver warnings on web
+  config.ignoreWarnings = [
+    /useNativeDriver/,
+    /RCTAnimation/,
+  ];
+  
   return config;
 };
