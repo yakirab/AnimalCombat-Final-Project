cf4d8ebc1e57c8c00827ef258dc1ad24714c24ff
yakirab <108821540+yakirab@users.noreply.github.com>
2025-08-17 16:26:21 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   
-  // Suppress useNativeDriver warnings on web
+  // Suppress useNativeDriver warnings on web.
   config.ignoreWarnings = [
     /useNativeDriver/,
     /RCTAnimation/,
